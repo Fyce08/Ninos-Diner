d0ab522fa94a125c63932f9021d79ef122ba3fba
Fyce08 <161514722+Fyce08@users.noreply.github.com>
2024-07-22 22:06:00 +0100

    QUERY_SQL.sql

--- QUERY_SQL.sql
+++ QUERY_SQL.sql
@@ -1,5 +1,4 @@
 -- Questions.
-
 -- 1. What is the total amount each customer spent at the restaurant?
 -- 2. How many days has each customer visited the restaurant?
 -- 3. What was the first item from the menu purchased by each customer?
